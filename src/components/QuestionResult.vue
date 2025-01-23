fe8619aa901b8cfa3f03eab0ffd979595c2a1df8
esttom <esttom42@gmail.com>
2025-01-23 23:55:09 +0900

    fix: question-result layout

--- src/components/QuestionResult.vue
+++ src/components/QuestionResult.vue
@@ -1,46 +1,45 @@
+<script setup lang="ts">
+const results = [
+  {
+    text: 'そこそこの頻度でやる',
+    percentage: 6,
+  },
+  {
+    text: '過去にやっていた',
+    percentage: 18,
+  },
+  {
+    text: '見ててやってみたいと思う',
+    percentage: 34,
+  },
+  {
+    text: '見てて良いゲームだと思う',
+    percentage: 43,
+  },
+]
+</script>
+
 <template>
-  <el-card class="progress-card" style="background-color: rgba(0,0,0,.08)">
-    <el-progress
-      :text-inside="true"
-      :stroke-width="20"
-      :percentage="6"
-      class="mb-2"
-      color="rgba(12,12,233,.32)"
-    >
-      <span>6%: そこそこの頻度でやる</span>
-    </el-progress>
-    <el-progress
-      :text-inside="true"
-      :stroke-width="20"
-      :percentage="18"
-      class="mb-2"
-      color="rgba(12,12,233,.32)"
-    >
-      <span>18%: 過去にやっていた</span>
-    </el-progress>
-    <el-progress
-      :text-inside="true"
-      :stroke-width="20"
-      :percentage="34"
-      class="mb-2"
-      color="rgba(12,12,233,.32)"
-    >
-      <span>34%: 見ててやってみたいと思う</span>
-    </el-progress>
-    <el-progress
-      :text-inside="true"
-      :stroke-width="20"
-      :percentage="43"
-      class="mb-1 mt-1"
-      color="rgba(12,12,233,.32)"
-    >
-      <span>43%: 見てて良いゲームだと思う</span>
-    </el-progress>
+  <el-card class="progress-card" style="background-color: rgba(255,255,255,.08)">
+    <div v-for="(result, idx) in results" :key="idx" justify="between" items="center" mb-2 w-full flex>
+      <el-progress
+        :text-inside="true"
+        :stroke-width="20"
+        :percentage="result.percentage"
+        class="flex-grow"
+        color="rgba(12,12,233,.32)"
+      >
+        <span>{{ result.text }}</span>
+      </el-progress>
+      <div w-42px text-right text-sm>
+        {{ `${result.percentage}%` }}
+      </div>
+    </div>
   </el-card>
 </template>
 
 <style scoped>
 .progress-card ::v-deep(.el-progress-bar__outer) {
-  background-color: rgba(45, 62, 102, 0.52);
+  background-color: rgba(45, 62, 102, 0.48);
 }
 </style>
